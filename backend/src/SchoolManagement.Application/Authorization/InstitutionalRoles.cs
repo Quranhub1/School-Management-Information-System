3b56303803f7a6111e8041370cc1158d30a71bdb
INSPIRED ENGINEER <kaigwaakram123@gmail.com>
2026-08-20 21:59:28 +0300

    feat: define institutional authorization roles

---
+++ backend/src/SchoolManagement.Application/Authorization/InstitutionalRoles.cs
@@ -0,0 +1,12 @@
+namespace SchoolManagement.Application.Authorization;
+
+public static class InstitutionalRoles
+{
+    public const string SystemAdministrator = "SystemAdministrator";
+    public const string Registrar = "Registrar";
+    public const string AcademicRegistrar = "AcademicRegistrar";
+    public const string FinanceOfficer = "FinanceOfficer";
+    public const string Lecturer = "Lecturer";
+    public const string ExaminationsOfficer = "ExaminationsOfficer";
+    public const string Student = "Student";
+}
