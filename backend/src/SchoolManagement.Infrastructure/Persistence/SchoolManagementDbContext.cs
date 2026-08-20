bbbf2af01665ac47c4f7ec8728bb0be080885d92
INSPIRED ENGINEER <kaigwaakram123@gmail.com>
2026-08-20 20:24:46 +0300

    feat: persist users roles and user-role mappings

--- backend/src/SchoolManagement.Infrastructure/Persistence/SchoolManagementDbContext.cs
+++ backend/src/SchoolManagement.Infrastructure/Persistence/SchoolManagementDbContext.cs
@@ -1,8 +1,41 @@
 using Microsoft.EntityFrameworkCore;
+using SchoolManagement.Domain.Identity;
 
 namespace SchoolManagement.Infrastructure.Persistence;
 
 public sealed class SchoolManagementDbContext(DbContextOptions<SchoolManagementDbContext> options)
     : DbContext(options)
 {
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Role> Roles => Set<Role>();
+    public DbSet<UserRole> UserRoles => Set<UserRole>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.HasIndex(x => x.Username).IsUnique();
+            entity.Property(x => x.Username).HasMaxLength(100).IsRequired();
+            entity.Property(x => x.PasswordHash).HasMaxLength(500).IsRequired();
+            entity.Property(x => x.FirstName).HasMaxLength(100).IsRequired();
+            entity.Property(x => x.LastName).HasMaxLength(100).IsRequired();
+            entity.Property(x => x.Email).HasMaxLength(254);
+        });
+
+        modelBuilder.Entity<Role>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.HasIndex(x => x.Name).IsUnique();
+            entity.Property(x => x.Name).HasMaxLength(100).IsRequired();
+            entity.Property(x => x.Description).HasMaxLength(500);
+        });
+
+        modelBuilder.Entity<UserRole>(entity =>
+        {
+            entity.HasKey(x => new { x.UserId, x.RoleId });
+            entity.HasOne<User>().WithMany().HasForeignKey(x => x.UserId).OnDelete(DeleteBehavior.Cascade);
+            entity.HasOne<Role>().WithMany().HasForeignKey(x => x.RoleId).OnDelete(DeleteBehavior.Cascade);
+        });
+    }
 }
