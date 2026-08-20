dbf3294e79f9e434a97aa30617876ff749d01e06
INSPIRED ENGINEER <kaigwaakram123@gmail.com>
2026-08-20 20:35:26 +0300

    feat: map students and guardians in persistence

--- backend/src/SchoolManagement.Infrastructure/Persistence/SchoolManagementDbContext.cs
+++ backend/src/SchoolManagement.Infrastructure/Persistence/SchoolManagementDbContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using SchoolManagement.Domain.Identity;
+using SchoolManagement.Domain.Students;
 
 namespace SchoolManagement.Infrastructure.Persistence;
 
@@ -9,6 +10,8 @@ public sealed class SchoolManagementDbContext(DbContextOptions<SchoolManagementD
     public DbSet<User> Users => Set<User>();
     public DbSet<Role> Roles => Set<Role>();
     public DbSet<UserRole> UserRoles => Set<UserRole>();
+    public DbSet<Student> Students => Set<Student>();
+    public DbSet<StudentGuardian> StudentGuardians => Set<StudentGuardian>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -36,6 +39,31 @@ public sealed class SchoolManagementDbContext(DbContextOptions<SchoolManagementD
             entity.HasKey(x => new { x.UserId, x.RoleId });
             entity.HasOne<User>().WithMany().HasForeignKey(x => x.UserId).OnDelete(DeleteBehavior.Cascade);
             entity.HasOne<Role>().WithMany().HasForeignKey(x => x.RoleId).OnDelete(DeleteBehavior.Cascade);
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.HasIndex(x => x.StudentNumber).IsUnique();
+            entity.Property(x => x.StudentNumber).HasMaxLength(50).IsRequired();
+            entity.Property(x => x.FirstName).HasMaxLength(100).IsRequired();
+            entity.Property(x => x.LastName).HasMaxLength(100).IsRequired();
+            entity.Property(x => x.OtherNames).HasMaxLength(200);
+            entity.Property(x => x.Gender).HasMaxLength(30);
+            entity.Property(x => x.NationalId).HasMaxLength(50);
+            entity.Property(x => x.PhoneNumber).HasMaxLength(30);
+            entity.Property(x => x.Email).HasMaxLength(254);
+        });
+
+        modelBuilder.Entity<StudentGuardian>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.HasIndex(x => x.StudentId);
+            entity.Property(x => x.FullName).HasMaxLength(200).IsRequired();
+            entity.Property(x => x.Relationship).HasMaxLength(50);
+            entity.Property(x => x.PhoneNumber).HasMaxLength(30);
+            entity.Property(x => x.Email).HasMaxLength(254);
+            entity.HasOne<Student>().WithMany().HasForeignKey(x => x.StudentId).OnDelete(DeleteBehavior.Cascade);
         });
     }
 }
