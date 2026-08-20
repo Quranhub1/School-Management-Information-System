9790ac414a393cfd5e62374e83d06ef303582c39
INSPIRED ENGINEER <kaigwaakram123@gmail.com>
2026-08-20 23:01:56 +0300

    feat: register module access service

--- backend/src/SchoolManagement.Application/DependencyInjection.cs
+++ backend/src/SchoolManagement.Application/DependencyInjection.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using SchoolManagement.Application.Academic;
 using SchoolManagement.Application.Authentication;
+using SchoolManagement.Application.Authorization;
 using SchoolManagement.Application.Students;
 
 namespace SchoolManagement.Application;
@@ -12,6 +13,7 @@ public static class DependencyInjection
         services.AddScoped<StudentService>();
         services.AddScoped<ProgrammeService>();
         services.AddScoped<AuthService>();
+        services.AddScoped<ModuleAccessService>();
         return services;
     }
 }
