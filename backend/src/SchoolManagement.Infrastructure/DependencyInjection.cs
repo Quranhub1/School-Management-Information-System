9d67d270421dfae1188352e16ee83e5e21290aef
INSPIRED ENGINEER <kaigwaakram123@gmail.com>
2026-08-22 13:39:07 +0300

    feat: register assessment repository

--- backend/src/SchoolManagement.Infrastructure/DependencyInjection.cs
+++ backend/src/SchoolManagement.Infrastructure/DependencyInjection.cs
@@ -22,6 +22,7 @@ public static class DependencyInjection
         services.AddScoped<IStudentRepository, StudentRepository>();
         services.AddScoped<IProgrammeRepository, ProgrammeRepository>();
         services.AddScoped<IAcademicRecordRepository, AcademicRecordRepository>();
+        services.AddScoped<IAssessmentRepository, AssessmentRepository>();
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<DatabaseHealthCheck>();
         services.AddSingleton<PasswordHasher>();
