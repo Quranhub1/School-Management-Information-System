c0143c82971ce75b001bf45165a542da5dedbe72
INSPIRED ENGINEER <kaigwaakram123@gmail.com>
2026-08-23 15:07:46 +0300

    feat: register student promotion service

--- backend/src/SchoolManagement.Application/DependencyInjection.cs
+++ backend/src/SchoolManagement.Application/DependencyInjection.cs
@@ -21,6 +21,7 @@ public static class DependencyInjection
         services.AddScoped<AssessmentService>();
         services.AddScoped<TranscriptStatusService>();
         services.AddScoped<ProgressionAssessment>();
+        services.AddScoped<StudentPromotionService>();
         services.AddScoped<AuthService>();
         services.AddScoped<AdministrationService>();
         services.AddScoped<ModuleAccessService>();
