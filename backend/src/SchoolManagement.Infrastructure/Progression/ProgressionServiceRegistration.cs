f226cf6b44ab9217af0ecb7b4225175fd055d55f
INSPIRED ENGINEER <kaigwaakram123@gmail.com>
2026-08-24 18:31:17 +0300

    chore: remove duplicate progression infrastructure registration

--- backend/src/SchoolManagement.Infrastructure/Progression/ProgressionServiceRegistration.cs
+++ backend/src/SchoolManagement.Infrastructure/Progression/ProgressionServiceRegistration.cs
@@ -1,14 +1,6 @@
-using Microsoft.Extensions.DependencyInjection;
-using SchoolManagement.Application.Progression;
-using SchoolManagement.Infrastructure.Repositories;
-
 namespace SchoolManagement.Infrastructure.Progression;
 
 public static class ProgressionServiceRegistration
 {
-    public static IServiceCollection AddProgressionInfrastructure(this IServiceCollection services)
-    {
-        services.AddScoped<ISemesterProgressionRepository, SemesterProgressionRepository>();
-        return services;
-    }
+    public static Microsoft.Extensions.DependencyInjection.IServiceCollection AddProgressionInfrastructure(this Microsoft.Extensions.DependencyInjection.IServiceCollection services) => services;
 }
