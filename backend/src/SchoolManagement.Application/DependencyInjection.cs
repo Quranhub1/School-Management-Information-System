06a5ec40b212b9eec27fbc2b0c81c52f299b74cd
INSPIRED ENGINEER <kaigwaakram123@gmail.com>
2026-08-24 15:28:15 +0300

    feat: register attendance application service

--- backend/src/SchoolManagement.Application/DependencyInjection.cs
+++ backend/src/SchoolManagement.Application/DependencyInjection.cs
@@ -2,6 +2,7 @@ using Microsoft.Extensions.DependencyInjection;
 using SchoolManagement.Application.Academic;
 using SchoolManagement.Application.Administration;
 using SchoolManagement.Application.Assessment;
+using SchoolManagement.Application.Attendance;
 using SchoolManagement.Application.Authentication;
 using SchoolManagement.Application.Authorization;
 using SchoolManagement.Application.Students;
@@ -22,6 +23,7 @@ public static class DependencyInjection
         services.AddScoped<TranscriptStatusService>();
         services.AddScoped<ProgressionAssessment>();
         services.AddScoped<StudentPromotionService>();
+        services.AddScoped<AttendanceService>();
         services.AddScoped<AuthService>();
         services.AddScoped<AdministrationService>();
         services.AddScoped<ModuleAccessService>();
