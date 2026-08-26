fe6eafb6c34ad3723a064990020ce636326f0bcf
INSPIRED ENGINEER <kaigwaakram123@gmail.com>
2026-08-26 11:52:47 +0300

    fix: add EF Core async extensions to payroll controller

--- backend/src/SchoolManagement.Api/Controllers/PayrollController.cs
+++ backend/src/SchoolManagement.Api/Controllers/PayrollController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SchoolManagement.Application.Authorization;
 using SchoolManagement.Application.Payroll;
 using SchoolManagement.Infrastructure.Persistence;
