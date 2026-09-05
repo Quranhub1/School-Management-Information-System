6c5357a1bd8e8c2f17d08da5f2f0feb3f19b2f46
INSPIRED ENGINEER <kaigwaakram123@gmail.com>
2026-09-05 18:48:54 +0300

    Add standard finance account code constants

---
+++ backend/src/SchoolManagement.Application/Finance/FinanceAccountCodes.cs
@@ -0,0 +1,10 @@
+namespace SchoolManagement.Application.Finance;
+
+public static class FinanceAccountCodes
+{
+    public const string StudentReceivables = "1100";
+    public const string Cash = "1010";
+    public const string Bank = "1020";
+    public const string MobileMoney = "1030";
+    public const string TuitionRevenue = "4100";
+}
