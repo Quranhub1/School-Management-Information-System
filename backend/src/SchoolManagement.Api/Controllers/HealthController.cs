a8871390b9e5b3822806d790f7d143a4a9f8b40c
INSPIRED ENGINEER <kaigwaakram123@gmail.com>
2026-09-08 22:34:42 +0300

    security: explicitly allow public API health endpoint

--- backend/src/SchoolManagement.Api/Controllers/HealthController.cs
+++ backend/src/SchoolManagement.Api/Controllers/HealthController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SchoolManagement.Api.Controllers;
@@ -6,6 +7,7 @@ namespace SchoolManagement.Api.Controllers;
 [Route("api/health")]
 public sealed class HealthController : ControllerBase
 {
+    [AllowAnonymous]
     [HttpGet]
     public IActionResult Get() => Ok(new
     {
