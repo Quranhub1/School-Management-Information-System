c6f761fa250e62b64af3e0ef2f3b655e00988264
INSPIRED ENGINEER <kaigwaakram123@gmail.com>
2026-09-08 13:04:42 +0300

    feat(hostel): add hostel authorization policy

--- backend/src/SchoolManagement.Application/Authorization/AuthorizationPolicies.cs
+++ backend/src/SchoolManagement.Application/Authorization/AuthorizationPolicies.cs
@@ -9,6 +9,7 @@ public static class AuthorizationPolicies
     public const string FinanceRead = "FinanceRead";
     public const string ExaminationManagement = "ExaminationManagement";
     public const string AttendanceManagement = "AttendanceManagement";
+    public const string HostelManagement = "HostelManagement";
     public const string ReportingManagement = "ReportingManagement";
     public const string CommunicationManagement = "CommunicationManagement";
     public const string CommunicationRead = "CommunicationRead";
@@ -24,6 +25,7 @@ public static class AuthorizationPolicies
         public static readonly string[] FinanceRead = [InstitutionalRoles.SystemAdministrator, InstitutionalRoles.FinanceOfficer, InstitutionalRoles.Principal, InstitutionalRoles.AssistantPrincipal, InstitutionalRoles.HeadOfDepartment, InstitutionalRoles.ResidentDirector, InstitutionalRoles.Secretary];
         public static readonly string[] ExaminationManagement = [InstitutionalRoles.SystemAdministrator, InstitutionalRoles.ExaminationsOfficer];
         public static readonly string[] AttendanceManagement = [InstitutionalRoles.SystemAdministrator, InstitutionalRoles.Lecturer, InstitutionalRoles.Registrar, InstitutionalRoles.AcademicRegistrar];
+        public static readonly string[] HostelManagement = [InstitutionalRoles.SystemAdministrator, InstitutionalRoles.HostelWarden, InstitutionalRoles.Registrar, InstitutionalRoles.ResidentDirector];
         public static readonly string[] AdmissionsManagement = [InstitutionalRoles.SystemAdministrator, InstitutionalRoles.Registrar, InstitutionalRoles.AcademicRegistrar];
         public static readonly string[] AdministrationManagement = [InstitutionalRoles.SystemAdministrator, InstitutionalRoles.Registrar];
         public static readonly string[] ReportingManagement = [InstitutionalRoles.SystemAdministrator, InstitutionalRoles.Registrar, InstitutionalRoles.AcademicRegistrar, InstitutionalRoles.FinanceOfficer, InstitutionalRoles.ExaminationsOfficer, InstitutionalRoles.Principal, InstitutionalRoles.ResidentDirector, InstitutionalRoles.AssistantPrincipal, InstitutionalRoles.HeadOfDepartment, InstitutionalRoles.Secretary];
