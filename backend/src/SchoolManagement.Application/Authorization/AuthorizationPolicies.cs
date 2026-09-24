f4917e433da0654ca72744cef0c18f301e5a6d3c
INSPIRED ENGINEER <kaigwaakram123@gmail.com>
2026-09-25 01:31:16 +0300

    Add welfare inventory authorization policy

--- backend/src/SchoolManagement.Application/Authorization/AuthorizationPolicies.cs
+++ backend/src/SchoolManagement.Application/Authorization/AuthorizationPolicies.cs
@@ -11,6 +11,7 @@ public static class AuthorizationPolicies
     public const string AttendanceManagement = "AttendanceManagement";
     public const string HostelManagement = "HostelManagement";
     public const string TransportManagement = "TransportManagement";
+    public const string InventoryManagement = "InventoryManagement";
     public const string ReportingManagement = "ReportingManagement";
     public const string CommunicationManagement = "CommunicationManagement";
     public const string CommunicationRead = "CommunicationRead";
@@ -28,6 +29,7 @@ public static class AuthorizationPolicies
         public static readonly string[] AttendanceManagement = [InstitutionalRoles.SystemAdministrator, InstitutionalRoles.Lecturer, InstitutionalRoles.Registrar, InstitutionalRoles.AcademicRegistrar];
         public static readonly string[] HostelManagement = [InstitutionalRoles.SystemAdministrator, InstitutionalRoles.HostelWarden, InstitutionalRoles.Registrar, InstitutionalRoles.ResidentDirector];
         public static readonly string[] TransportManagement = [InstitutionalRoles.SystemAdministrator, InstitutionalRoles.TransportOfficer, InstitutionalRoles.Registrar, InstitutionalRoles.ResidentDirector];
+        public static readonly string[] InventoryManagement = [InstitutionalRoles.SystemAdministrator, InstitutionalRoles.StoreOfficer];
         public static readonly string[] AdmissionsManagement = [InstitutionalRoles.SystemAdministrator, InstitutionalRoles.Registrar, InstitutionalRoles.AcademicRegistrar];
         public static readonly string[] AdministrationManagement = [InstitutionalRoles.SystemAdministrator, InstitutionalRoles.Registrar];
         public static readonly string[] ReportingManagement = [InstitutionalRoles.SystemAdministrator, InstitutionalRoles.Registrar, InstitutionalRoles.AcademicRegistrar, InstitutionalRoles.FinanceOfficer, InstitutionalRoles.ExaminationsOfficer, InstitutionalRoles.Principal, InstitutionalRoles.ResidentDirector, InstitutionalRoles.AssistantPrincipal, InstitutionalRoles.HeadOfDepartment, InstitutionalRoles.Secretary];
